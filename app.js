4967f033f5b9051f311701032c3dc97eb21ffc21
Tristverm <emmanuelkyalo72@gmail.com>
2023-04-10 18:23:36 +0300

    Added Few chnages on code styling

--- app.js
+++ app.js
@@ -3,36 +3,28 @@ const copyRightDate = document.getElementById("copyRight");
 const menuButton = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
-
-
-                        //Functions
+//Functions
 //Normal Functions
 const getCurrentYear = () => {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    console.log(currentYear)
-    return currentYear.toString();
-  }
-  
-  copyRightDate.innerHTML =`<span class="text-white">&copy; ${getCurrentYear()} Loopstudios. All Rights Reserved</span>`;
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  console.log(currentYear);
+  return currentYear.toString();
+};
 
 const menuTransition = () => {
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("flex");
+};
 
-    menu.classList.toggle("hidden");
-    menu.classList.toggle("flex")
-
-}
-
-  // Function Handlers
+// Function Handlers
 const menuBtnFlipHandler = () => {
-  menuButton.classList.toggle("open")
+  menuButton.classList.toggle("open");
   menuTransition();
 };
 
-
-
-
-
-
 //eventListener
-menuButton.addEventListener("click", menuBtnFlipHandler)
+menuButton.addEventListener("click", menuBtnFlipHandler);
+
+// Code
+copyRightDate.innerHTML = `<span class="text-white">&copy; ${getCurrentYear()} Loopstudios. All Rights Reserved</span>`;
